10bab3f8be1824000b9cddb1aa796e9e575b4799
Agesta Saputra <agesta.budy@mekari.com>
2022-04-13 19:24:23 +0700

    feat: create custom layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,92 @@
 <template>
-  <Tutorial/>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.food"
+          :options="foods"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          id="checkboxes-4"
+          :aria-describedby="ariaDescribedby"
+        >
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    name: 'IndexPage',
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: [],
+          isLoading: false
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true,
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        if (event) event.preventDefault()
+        this.form.isLoading = true
+        setTimeout(() => {
+          this.form.isLoading = false
+          this.onReset()
+        }, 2000);
+      },
+      onReset(event) {
+        if (event) event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+      }
+    }
+  }
 </script>
